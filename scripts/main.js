e74094870a5ecd83bc1b2757fbf485039c141cbf
Noah Walji <noahwalji@gmail.com>
2022-03-26 16:53:21 -0400

    Removed WebSocket. Made live messaging setTimeout

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,7 @@
 $(document).ready(function() {
     var value = "";
+    var myInterval;
     jQuery(function($) {
-        var webSocket = new WebSocket("ws://localhost");
-        webSocket.onerror = function(e) {
-
-        }
-        webSocket.onmessage = function(e) {
-            var json = JSON.parse(JSON.parse(e.data));
-            if (json.type == "message") {
-                // Get New Message if On Same Channel
-                if (value == json.channel) {
-                    $(".messagesGroup").append("<li class='list-group-item'><img src='"+json.profile_image+"' width='50' height='50'>"
-                    +json.author+" | "+json.time+"<br>"
-                    +json.message+"</li>")
-                }
-            }
-
-            else if (json.type == "channel") {
-                $(".channelList").append("<button type='button' class='channelItem list-group-item list-group-item-action'>"+json.name+"</button>")
-            }
-
-        }
         $("#sendMessage").submit(function( event ) {
             event.preventDefault();
             $("#error-message").hide();
@@ -29,15 +10,6 @@ $(document).ready(function() {
                 return;
             }
 
-            webSocket.send(JSON.stringify({
-                'type': "message",
-                'author': $("#userNameValue").text(),
-                'uid': $("#uidValue").text(),
-                'channel':value,
-                'message':$("#message").val(),
-                "profile_image":$("#pfpValue").text(),
-                "time": new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
-            }))
             $.ajax({
                 url: "classes/Ajax.php",
                 type: "POST",
@@ -57,10 +29,6 @@ $(document).ready(function() {
 
           $("#addChannelForm").submit(function (event) {
             event.preventDefault();
-            webSocket.send(JSON.stringify({
-              'type': "channel",
-              'name': $("#channelName").val(),
-            }))
             this.submit();
         });
 
@@ -83,22 +51,14 @@ $(document).ready(function() {
     $(document).on('click', '.channelItem',function() {
         value = $(this).text();
         $(this).addClass('active').siblings().removeClass('active');
-        $.ajax({
-            url: "classes/Ajax.php",
-            type: "POST",
-            data: {ajaxCall: "getMessages", channelName: $(this).text()},
-            success: function(response){
-                var messages = JSON.parse(response);
+        if (myInterval) {
+            console.log("Clearing")
+            clearInterval(myInterval);
+            myInterval = null;
+        }
+        loadMessages($(this).text());
+        myInterval = setInterval(loadMessages, 5000, $(this).text());
 
-
-                $(".messagesGroup").empty();
-                for (var message of messages) {
-                    $(".messagesGroup").append("<li class='list-group-item'><img src='"+message.profile_image+"' width='50' height='50'>"
-                    +message.nickname+" | "+message.time+"<br>"
-                    +message.contents+"</li>")
-                }
-            }
-        });
     });
 
 
@@ -131,6 +91,26 @@ $(document).ready(function() {
 
 });
 
-function previewFile(input){
+function loadMessages(channel){
+    console.log(channel);
+    $.ajax({
+        url: "classes/Ajax.php",
+        type: "POST",
+        data: {ajaxCall: "getMessages", channelName: channel},
+        success: function(response){
+            var messages = JSON.parse(response);
+            var list = "";
+            for (var message of messages) {
+                list = list + "<li class='list-group-item'><img src='"+message.profile_image+"' width='50' height='50'>"
+                +message.nickname+" | "+message.time+"<br>"
+                +message.contents+"</li>";
+            }
+            if ($(".messagesGroup").html() != list) {
+                $(".messagesGroup").empty();
+                $(".messagesGroup").append(list);
+            }
+
+        }
+    });
 
 }
